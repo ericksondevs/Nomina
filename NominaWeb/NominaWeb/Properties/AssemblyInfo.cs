7f26c4c71cdd346a6564704c864aec076f31c522
Kelvin O. Valenzuela R <Kelvinvr@KELVINVR-PC.Litesyt.local>
2019-03-06 11:36:10 -0400

    Comienzo de Proyecto (Me daba error al correrlo)

--- NominaWeb/NominaWeb/Properties/AssemblyInfo.cs
+++ NominaWeb/NominaWeb/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NominaWeb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NominaWeb")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("afa609bb-71fc-4660-84dd-57efce4ec392")]
+[assembly: Guid("d9c9256b-7841-4fae-8148-a7eef1e3e255")]
 
 // Version information for an assembly consists of the following four values:
 //
